4c26210b10a2ece8c47e6b75ec5b79c840731812
tomermayost <tmayost@gmail.com>
2022-03-12 12:37:29 +0200

    Create tests env and e2e tests

--- README.md
+++ README.md
@@ -1,35 +1,98 @@
 
 ## Description
-Activity Monitor Exercise running with [Nest](https://github.com/nestjs/nest) framework TypeScript starter repository.
+Activity Monitor running with [Nest](https://github.com/nestjs/nest) framework, Redis and MongoDB.
 
-## Installation
 
-```bash
-$ npm install
+## Rest API: 
+
+```
+1. Create Event 
+Request:
+method: POST
+URL: 'http://{host}:{port}/api/monitor/events' 
+Content-Type: application/json
+Body: 
+{
+    "type": "click",
+    "user": "tom12345",
+    "page": "gooe.com",
+    "element_id": "1erv2333"
+}
+
+Response:
+Http Status: 201 (created)
+Body:
+{
+    "type": "click",
+    "user": "tom12345",
+    "page": "gooe.com",
+    "element_id": "1erv2333",
+    "timestamp": "2022-03-12T10:06:52.218Z"
+}
+```
+```
+2. Get Uses Visits since start of day
+Request:
+method: GET
+URL: 'http://{host}:{port}/api/monitor/user/visits' 
+
+Response:
+Http Status: 200 (ok)
+Body:
+{
+    "users_visiting_today": 2022
+}
+```
+```
+3. Get Events per user since start of day (ordered by events descending)
+Request:
+method: GET
+Optional Query params: skip, limit
+URL: 'http://{host}:{port}/api/monitor/events/user' 
+
+Response:
+Http Status: 200 (ok)
+Body:
+[
+        { "events": 30, "user": 'jack' },
+        { "events": 25, "user": 'jim' },
+        { "events": 20, "user": 'jill' },
+        { "events": 15, "user": 'suzy' },
+        { "events": 10, "user": 'moshe' },
+        { "events": 5, "user": 'clod' }
+]
+```
+```
+4. Get Average Events per minute since start of day
+Request:
+method: GET
+URL: 'http://{host}:{port}/api/monitor/events/avg_epm' 
+
+Response:
+Http Status: 200 (ok)
+Body:
+{
+    "avg_epm": 4.7777777
+}
 ```
 
-## Running the app
+## Tests
+```
+to run the tests:
+$npm run test:e2e
+```
 
+## Running the app with docker
+    it will take a few minutes to build the image the first time you run it.
 ```bash
 # development
-$ npm run start
-
-# watch mode
-$ npm run start:dev
-
-# production mode
-$ npm run start:prod
+$ docker-compose up -d main 
 ```
+    this will spin up the app and its dependecies in docker containers
+## Running the app in localy in dev mode 
+    for this to work you need to replace the hosts names to localhost in the .env file for the DATABASE_URL and REDIS_HOST vars
 
-## Test
 
-```bash
-# unit tests
-$ npm run test
-
-# e2e tests
-$ npm run test:e2e
-
-# test coverage
-$ npm run test:cov
+```
+$ docker-compose up -d redis mongodb && npm run start:dev
 ```
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,31 +1,234 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import { Test } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as pactum from 'pactum';
 import { AppModule } from './../src/app.module';
+import { DatabaseService } from '../src/database/database.service';
+import { EventDto } from '../src/monitor/dto';
 
-
-describe('AppController (e2e)', () => {
+describe('App e2e', () => {
   let app: INestApplication;
-
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+  let dbService: DatabaseService;
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule]
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
     await app.init();
+    await app.listen(3333);
+
+    dbService = app.get(DatabaseService);
+    await dbService.clearDB();
+
+    pactum.request.setBaseUrl('http://localhost:3333/')
+
+    dbService.clearDB()
   });
 
-  it('register event', () => {
-    return request(app.getHttpServer())
-      .post('/api/monitor/event')
-      .send({
-        type: "login",
-        page: "google.com",
-        user: "john",
-        element_id: "1234"
+  it(`create event for user john via post request - 
+      app puts the event in the work queue`, () => {
+    const dto: EventDto = {
+      type: 'type',
+      page: 'page',
+      element_id: 'element_id',
+      user: 'john',
+      timestamp: null
+    }
+    return pactum.spec()
+      .post('api/monitor/events')
+      .withBody(dto)
+      .expectStatus(201)
+      .expectJsonLike({
+        type: 'type',
+        page: 'page',
+        element_id: 'element_id',
+        user: 'john',
       })
-      .expect(201);
+  })
+
+  it('wait for consumer to pick up the event and then db should have 1 event', async () => {
+
+    await pactum.sleep(100);
+
+    return pactum.spec()
+      .get('api/monitor/events/user')
+      .expectStatus(200)
+      .expectJson([{ "events": 1, "user": "john" }])
+  })
+
+  it(`create 9 more events for john - 
+      then get events per user should return total 10 events`, async () => {
+    for (let i = 0; i < 9; i++) {
+      await dbService.createEvent({
+        type: 'type',
+        page: 'page',
+        element_id: 'element_id',
+        user: 'john',
+        timestamp: new Date()
+      })
+    }
+
+    return pactum.spec()
+      .get('api/monitor/events/user')
+      .expectStatus(200)
+      .expectJson([{ "events": 10, "user": "john" }])
+  })
+
+  it(`clear db, create diferent number of events for 5 users
+      then get events per user should be ordered decending`, async () => {
+    await dbService.clearDB()
+
+    await createEventsForUser('jim', 25, dbService)
+    await createEventsForUser('clod', 5, dbService)
+    await createEventsForUser('jack', 30, dbService)
+    await createEventsForUser('jill', 20, dbService)
+    await createEventsForUser('moshe', 10, dbService)
+    await createEventsForUser('suzy', 15, dbService)
+
+    return pactum.spec()
+      .get('api/monitor/events/user')
+      .expectStatus(200)
+      .expectJsonLike([
+        { "events": 30, "user": 'jack' },
+        { "events": 25, "user": 'jim' },
+        { "events": 20, "user": 'jill' },
+        { "events": 15, "user": 'suzy' },
+        { "events": 10, "user": 'moshe' },
+        { "events": 5, "user": 'clod' }
+      ])
+  })
+
+  it(`we just created 105 events way less the a minute so
+      get avg events per minute - should be 105`, async () => {
+    return pactum.spec()
+      .get('api/monitor/events/avg_epm')
+      .expectStatus(200)
+      .expectJson({ "avg_epm": 105 })
+  })
+
+  it(`cleareDb, create 30 events per minute for 5 differen users
+      then get avg epm should return 30`, async () => {
+
+    await dbService.clearDB()
+
+    for (let i = 0; i < 5; i++) {
+      for (let j = 0; j < 30; j++) {
+        const timestamp = new Date()
+        timestamp.setMinutes(i)
+        timestamp.setSeconds(i)
+        await dbService.createEvent({
+          type: 'type',
+          page: 'page',
+          element_id: 'element_id',
+          user: 'john' + i,
+          timestamp: timestamp
+        })
+      }
+    }
+
+    return pactum.spec()
+      .get('api/monitor/events/avg_epm')
+      .expectStatus(200)
+      .expectJson({ "avg_epm": 30 })
+  })
+
+  it(`now we should get 30 events per user `, async () => {
+
+    return pactum.spec()
+      .get('api/monitor/events/user')
+      .expectStatus(200)
+      .expectJsonLike([
+        { "events": 30, "user": /john[0-4]/ },
+        { "events": 30, "user": /john[0-4]/ },
+        { "events": 30, "user": /john[0-4]/ },
+        { "events": 30, "user": /john[0-4]/ },
+        { "events": 30, "user": /john[0-4]/ }
+      ])
+  })
+
+  it('get users visiting today - should be 5 based on previous test', async () => {
+    return pactum.spec()
+      .get('api/monitor/user/visits')
+      .expectStatus(200)
+      .expectJson({ "users_visiting_today": 5 })
+  })
+
+  it(`clear db, create events today and events yesterday
+      make sure only today's events are counted`, async () => {
+    await dbService.clearDB()
+
+    const yesterday = new Date()
+    yesterday.setHours(-25)
+
+    await createEventsForUser('jim', 25, dbService, yesterday)
+    await createEventsForUser('clod', 5, dbService)
+    await createEventsForUser('jack', 30, dbService)
+
+    return pactum.spec()
+      .get('api/monitor/user/visits')
+      .expectStatus(200)
+      .expectJson({ "users_visiting_today": 2 })
+
+  })
+
+  it('events per user should disregard user jim', async () => {
+    return pactum.spec()
+      .get('api/monitor/events/user')
+      .expectStatus(200)
+      .expectJsonLike([
+        { "events": 30, "user": 'jack' },
+        { "events": 5, "user": 'clod' },
+      ])
+  })
+
+  it('evg epm should be 35 - disregarding jim yesterday', async () => {
+    return pactum.spec()
+      .get('api/monitor/events/avg_epm')
+      .expectStatus(200)
+      .expectJson({ "avg_epm": 35 })
+  })
+
+  it('clear db, test apis on empty db - edge cases', async () => {
+    await dbService.clearDB()
+  })
+
+  it('avg epm', async () => {
+    return pactum.spec()
+      .get('api/monitor/events/avg_epm')
+      .expectStatus(200)
+      .expectJson({ "avg_epm": 0 })
+  })
+
+  it('events per user', async () => {
+    return pactum.spec()
+      .get('api/monitor/events/user')
+      .expectStatus(200)
+      .expectJsonLike([])
+  })
+
+  it('users visiting today', async () => {
+    return pactum.spec()
+      .get('api/monitor/user/visits')
+      .expectStatus(200)
+      .expectJson({ "users_visiting_today": 0 })
+  })
+
+  afterAll(() => {
+    app.close();
   });
+
 
 });
+
+async function createEventsForUser(user, numEvent, dbService, timestamp?) {
+  for (let i = 0; i < numEvent; i++) {
+    await dbService.createEvent({
+      type: 'type',
+      page: 'page',
+      element_id: 'element_id',
+      user: user,
+      timestamp: timestamp ? timestamp : new Date()
+    })
+  }
+} 
